407f5d92123deb24a62795b37a78e7cf2032be1d
taniasrt8 <52179438+taniasrt8@users.noreply.github.com>
2019-06-25 15:46:14 -0700

    Add files via upload
    Python bible tasks

---
+++ task_2.py
@@ -0,0 +1,16 @@
+# Create a variable called name and set it equal to your name
+name = "Tania"
+
+# Create a variable called age and set it to your age in years 
+# Note: This must be an int, not a string!
+age = 28
+
+# Create a variable called place and set it equal to where you live
+place = "Mountain View"
+
+# Create a variable called output, and use {} symbols and the format() function to 
+# make it contain a string like the example text in the description
+output = "Hello my name is {} and I am {} years old. I live in {} and I love Python!".format(name, age, place)
+
+# Finally, use the print() function to print the output.
+print(output)
